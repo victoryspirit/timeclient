4fdae6d2c717d2942a8aaba12dfd6e47f9870547
Viktoriia Korsak <korsak.viktoriia@gmail.com>
2021-02-07 23:53:56 +0100

    Add TimeClient for socket connection

---
+++ TimeClient/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TimeClient
+{
+    class Program
+    {
+        private static Socket _clientSocket =
+            new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
+
+        private static int PORT = 55555;
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            LoopConnect();
+            SendLoop();
+            Console.ReadLine();
+        }
+
+        private static void SendLoop()
+        {
+            while (true)
+            {
+                Thread.Sleep(5000);
+                byte[] buffer = Encoding.ASCII.GetBytes("get time");
+                _clientSocket.Send(buffer);
+
+                byte[] receivedBuf = new byte[1024];
+                int rec = _clientSocket.Receive(receivedBuf);
+                byte[] data = new byte[rec];
+                Array.Copy(receivedBuf, data, rec);
+                Console.WriteLine("Received: " + Encoding.ASCII.GetString(data));
+            }
+        }
+
+        private static void LoopConnect()
+        {
+            int attempts = 0;
+
+            while (!_clientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    _clientSocket.Connect(IPAddress.IPv6Loopback, PORT);
+
+                }
+                catch (SocketException)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Connection attempts: " + attempts.ToString());
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Connected");
+        }
+    }
+}
